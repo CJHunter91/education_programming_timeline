9b6ce96728ac503f5b2a2e314500085856e849ed
Gergo Nagy <guzmop@gmail.com>
2017-09-15 15:38:19 +0100

    frontend server set up

--- client/src/app.js
+++ client/src/app.js
@@ -3,7 +3,7 @@ var AjaxRequest = require('./services/ajax_request');
 
 var app = function(){
     
-    var ajaxRequest = new AjaxRequest('http://localhost:3000/api/languages');
+    var ajaxRequest = new AjaxRequest('http://localhost:3001/api/languages');
 }
 
 window.addEventListener('load', app);
