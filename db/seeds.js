8c5d3b4a484c0660aee2339b998e72a21def3e71
Chris Hunter <orkem2@gmail.com>
2017-09-15 14:04:50 +0100

    adds rough db data stucture

---
+++ db/seeds.js
@@ -0,0 +1,23 @@
+//use collection name
+
+//insert 
+
+var languageData = [
+{
+  id: 1,
+  language: "Ruby",
+  author: "matz",
+  description: "Lorem Ipsum",
+  year: "1995",
+  usedBy: "FreeAgent",
+  usedFor: "Web applications",
+  exampleCode: "puts 'hello world' ",
+  pros: ["easy to use and learn"],
+  cons: ["not as efficent"],
+  popularity: {
+    1996: 100
+  }
+  documentation: ["https://ruby-doc.org/core-2.4.1/"]
+  links: []
+}
+];
