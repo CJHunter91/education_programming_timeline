c36f011b5d161d8bcca5daed11ca3a7109a63a84
Stephen Beattie <beattie19@hotmail.co.uk>
2017-09-15 15:00:32 +0100

    added index.js into controllers and set up server.js

---
+++ controllers/index.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var router = express.Router();
+
+// router.use('/api/languages'), require('./languages')
+
+router.get('/', function(req, res){
+    res.json
+} )
+
+module.exports = router;
