6641d4298d02b2e442a4239b963aef03f4af158b
Gergo Nagy <guzmop@gmail.com>
2017-09-16 16:26:07 +0100

    add icon to sidebar

--- client/src/views/languages_view.js
+++ client/src/views/languages_view.js
@@ -8,7 +8,8 @@ LanguagesView.prototype.render = function(data) {
     var ul = document.createElement('ul');
     data.forEach(function(language) {
         var li = document.createElement('li');
-        li.innerHTML = `<a href="/" class="btn btn-default"> ${language.language} </a>`;
+        li.innerHTML = `
+        <a href="/" class="btn btn-default"> ${language.language} <img src="https://cdn0.iconfinder.com/data/icons/octicons/1024/ruby-128.png"></a>`;
         ul.appendChild(li);
     });
     section.appendChild(ul);
