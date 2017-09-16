0fd3fea3596fae67d0b79ecb7a322cbc30a087e5
Travis Grimmond <travisslack1994@outlook.com>
2017-09-16 12:30:30 +0100

    add render to Ajax get request, requires language view

--- client/src/app.js
+++ client/src/app.js
@@ -1,13 +1,16 @@
 var AjaxRequest = require('./services/ajax_request');
-
+var LanguageView = require('./views/languages_view');
 
 var app = function(){
     
     var ajaxRequest = new AjaxRequest('http://localhost:3000/api/languages');
+    var languageView = new LanguageView();
     //call view pass 
 
+
     //pass the get a view callback
-    ajaxRequest.get();
+    ajaxRequest.get(languageView.render);
+    
 }
 
 window.addEventListener('load', app);
