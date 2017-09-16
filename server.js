86970870cdfec6ad5a102b503eb91d34c18c5959
Chris Hunter <orkem2@gmail.com>
2017-09-16 11:33:38 +0100

    adds code to allow requests to backend

--- server.js
+++ server.js
@@ -2,6 +2,13 @@ var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 
+// two different servers so you need this
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
