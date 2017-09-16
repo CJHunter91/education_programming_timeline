25e51bd4e5697a03f0ce5272671677216f729281
Travis Grimmond <travisslack1994@outlook.com>
2017-09-16 12:21:53 +0100

    creates render in languages view file, adds section to html file

--- client/src/app.js
+++ client/src/app.js
@@ -4,7 +4,9 @@ var AjaxRequest = require('./services/ajax_request');
 var app = function(){
     
     var ajaxRequest = new AjaxRequest('http://localhost:3000/api/languages');
+    //call view pass 
 
+    //pass the get a view callback
     ajaxRequest.get();
 }
 
