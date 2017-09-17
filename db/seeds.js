1785a250bb294d986bfa1d5dbbf8d8135a8d54cf
Chris Hunter <orkem2@gmail.com>
2017-09-17 13:44:26 +0100

    added c, c# and php seed data, also changed icon to be dynamically set in language view

--- db/seeds.js
+++ db/seeds.js
@@ -9,7 +9,7 @@ var languageData = [
   language: "Ruby",
   author: "matz",
   description: "Lorem Ipsum",
-  year: "1995",
+  year: "1993",
   usedBy: "FreeAgent",
   usedFor: "Web applications",
   exampleCode: "puts 'hello world' ",
@@ -20,43 +20,43 @@ var languageData = [
   },
   documentation: ["https://ruby-doc.org/core-2.4.1/"],
   links: ["lorem"],
-  icon: ""
+  icon: "https://upload.wikimedia.org/wikipedia/commons/7/73/Ruby_logo.svg"
 },
 {
   id: 2,
-  language: "",
-  author: "",
-  description: "",
-  year: "",
-  usedBy: "",
-  usedFor: "",
-  exampleCode: " ",
-  pros: [""],
-  cons: [""],
+  language: "PHP",
+  author: "Rasmus Ledorf",
+  description: "PHP is a widely-used open source general-purpose scripting language that is especially suited for web development and can be embedded into HTML",
+  year: "1995",
+  usedBy: "Facebook, Wikipedia, Digg and Wordpress",
+  usedFor: "Creating/Maintaining dynamic webpages and server-side development",
+  exampleCode: "echo 'hello world'",
+  pros: ["Open source (free to use), relatively efficent and works on most platforms."],
+  cons: ["Security due to being open-source anyone can view the source code and find weaknesses, Not suitable for larger applications due to lack of modularity."],
   popularity: {
     1996: 100
   },
-  documentation: [""],
+  documentation: ["http://php.net/docs.php"],
   links: [""],
-  icon: ""
+  icon: "https://upload.wikimedia.org/wikipedia/commons/2/27/PHP-logo.svg"
 },
 {
   id: 3,
-  language: "",
-  author: "",
-  description: "",
-  year: "",
-  usedBy: "",
-  usedFor: "",
-  exampleCode: " ",
-  pros: [""],
-  cons: [""],
+  language: "C",
+  author: "Dennis Ritchie",
+  description: "General-purpose low level language that a lot of current languages are derivatives of, created for UNIX systems",
+  year: "1972",
+  usedBy: "UNIX, embedded systems",
+  usedFor: "Cross-platform programming, system programming, UNIX programming",
+  exampleCode: "#include<stdio.h>main(){printf('Hello World');}",
+  pros: ["Small clean language, very powerful, can be used in C++ programs"],
+  cons: ["Difficult to learn, code can get messy quickly, needs to be compiled"],
   popularity: {
     1996: 100
   },
-  documentation: [""],
+  documentation: ["http://devdocs.io/c/"],
   links: [""],
-  icon: ""
+  icon: "https://upload.wikimedia.org/wikipedia/commons/3/35/The_C_Programming_Language_logo.svg"
 },
 {
   id: 4,
@@ -114,21 +114,21 @@ var languageData = [
 },
 {
   id: 7,
-  language: "",
-  author: "",
-  description: "",
-  year: "",
-  usedBy: "",
-  usedFor: "",
+  language: "C#",
+  author: "Anders Hejlsberg (Microsoft)",
+  description: "C# is a general-purpose, object-oriented programming language developed by Microsoft.",
+  year: "2000",
+  usedBy: "VisualStudio, Stack Overflow",
+  usedFor: "Can be used with Microsofts .NET framework and is felxible enough to be used in most areas of software development",
   exampleCode: " ",
-  pros: [""],
-  cons: [""],
+  pros: ["Can be statically or dynamically typed, similiar syntax to other c languages, fully integrated with the .NET library."],
+  cons: ["Less efficent as it is interpreted"],
   popularity: {
     1996: 100
   },
-  documentation: [""],
+  documentation: ["https://docs.microsoft.com/en-us/dotnet/csharp/"],
   links: [""],
-  icon: ""
+  icon: "https://upload.wikimedia.org/wikipedia/commons/0/0d/C_Sharp_wordmark.svg"
 },
 {
   id: 8,
