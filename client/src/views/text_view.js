61f2887676bc8254d0677d2e78ac5625af8ad6cd
Stephen Beattie <beattie19@hotmail.co.uk>
2017-09-18 16:53:10 +0100

    added new append in text view so chart can be added to front page, updated the dataFill method so that it will work for an array or language instead of just one

--- client/src/views/text_view.js
+++ client/src/views/text_view.js
@@ -5,12 +5,15 @@ TextView.prototype.render = function(data) {
   var div = document.querySelector("#main-view");
   var heading = document.createElement('h3');
   var paragraph = document.createElement('p');
+  var fullChart = document.createElement('section')
 
   heading.innerText = data[0].title;
   paragraph.innerText = data[0].text;
+  fullChart.id = "fullChart";
 
   div.appendChild(heading);
   div.appendChild(paragraph);
+  div.appendChild(fullChart);
 };
 
 module.exports = TextView;
